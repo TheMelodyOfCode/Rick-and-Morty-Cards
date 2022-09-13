4eea5374b9affb811d0dd367dcf191e7676adfc0
D.Cameron <lobbygow@posteo.de>
2022-09-13 12:41:23 +0200

    converted class compnt to funct. compnt

--- client/src/components/card/card.component.jsx
+++ client/src/components/card/card.component.jsx
@@ -1,22 +1,17 @@
-import { Component } from "react";
 import './card.styles.css';
 
+const Card  = ({users })=> {
 
-class Card  extends Component {
-    render() {
-        /* destructering and passing variables through props */
-        const { username, bio, photo, userid } = this.props.users;
-        return (
+    const {username, bio, photo, userid } = users;
 
+    return (
         <div className='card-container' key={userid}>
-            <img className="image" src={`./img/${photo}`}  alt={username} />
-            <div>{username} </div>
-                <div>{bio} </div>
+        <img className="image" src={`./img/${photo}`}  alt={username} />
+        <div>{username} </div>
+            <div>{bio} </div>
+    </div>
+    );
 
-        </div>
-
-        )
-    }
 }
 
 export default Card;
--- client/src/components/search-box/search-box.component.jsx
+++ client/src/components/search-box/search-box.component.jsx
@@ -1,26 +1,14 @@
-import { Component } from "react";
 
-/* all CSS is bound to getter and can be accesseed everywhere
-to write import here is just to have an overview where the file lives for the developer
-to isolate css in we need to learn css in js libraries */
 import './search-box.styles.css';
 
-class SearchBox extends Component {
-
-    render () {
-
-        return (
-            <input  
-
-                className={`search-box  ${this.props.className}`}
-                type='search' 
-                placeholder= {this.props.placeholder}
-                //we are passing an event handler here
-                onChange={this.props.onChangeHandler} 
-            />
-
-        )
-    }
-}
+const SearchBox = ({className, placeholder, onChangeHandler}) => (
+    <input  
+        className={`search-box  ${className}`}
+        type='search' 
+        placeholder= {placeholder}
+        //we are passing an event handler here
+        onChange={onChangeHandler} 
+    />
+);
 
 export default SearchBox;
